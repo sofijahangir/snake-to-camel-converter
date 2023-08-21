99ca41f76d21c359321284018e634dba6d13e562
Jahangir Ahmad <jahangir@studio137.co.za>
2023-08-21 10:28:42 +0530

    Kebab Case and Pascel Case

--- Readme.md
+++ Readme.md
@@ -13,15 +13,43 @@ npm install snake-to-camel-converter
 ```javascript
 const snakeToCamelConverter = require('snake-to-camel-converter');
 
+// String Case Conversions
+const inputString = "this_is_a_test_string";
+
+const camelCaseString = snakeToCamelConverter.convertToCamelCase(inputString);
+console.log(camelCaseString);
+
+const snakeCaseString = snakeToCamelConverter.convertToSnakeCase(camelCaseString);
+console.log(snakeCaseString);
+
+const pascalCaseString = snakeToCamelConverter.convertToPascalCase(snakeCaseString);
+console.log(pascalCaseString);
+
+const kebabCaseString = snakeToCamelConverter.convertToKebabCase(pascalCaseString);
+console.log(kebabCaseString);
+
+// Object Key Conversions
 const inputObject = {
-  my_key: 'value',
-  nested_object: {
-    another_key: 'another_value'
+  first_name: 'John',
+  last_name: 'Doe',
+  contact_info: {
+    email_address: 'john@example.com',
+    phone_number: '123-456-7890'
   }
 };
 
-const camelCaseObject = snakeToCamelConverter(inputObject);
+const camelCaseObject = snakeToCamelConverter.convertToCamelCase(inputObject);
 console.log(camelCaseObject);
+
+const snakeCaseObject = snakeToCamelConverter.convertToSnakeCase(camelCaseObject);
+console.log(snakeCaseObject);
+
+const pascalCaseObject = snakeToCamelConverter.convertToPascalCase(snakeCaseObject);
+console.log(pascalCaseObject);
+
+const kebabCaseObject = snakeToCamelConverter.convertToKebabCase(pascalCaseObject);
+console.log(kebabCaseObject);
+
 ```
 ## Example 
 
--- index.js
+++ index.js
@@ -1,22 +1,74 @@
-function convertToCamelCase(obj) {
-  if (Array.isArray(obj)) {
-    return obj.map(item => convertToCamelCase(item));
-  } else if (typeof obj === 'object' && obj !== null) {
+function convertToCamelCase(input) {
+  if (typeof input === 'string') {
+    return input.replace(/([-_]\w)/g, match => match.charAt(1).toUpperCase());
+  } else if (Array.isArray(input)) {
+    return input.map(item => convertToCamelCase(item));
+  } else if (typeof input === 'object' && input !== null) {
     let newObj = {};
-    for (let key in obj) {
+    for (let key in input) {
       let newKey = key.replace(/_([a-z])/g, (match, group1) => group1.toUpperCase());
-      newObj[newKey] = convertToCamelCase(obj[key]);
+      newObj[newKey] = convertToCamelCase(input[key]);
     }
     return newObj;
   } else {
-    return obj;
+    return input;
   }
 }
 
-module.exports = function (input) {
-  if (Array.isArray(input)) {
-    return input.map(item => convertToCamelCase(item));
+function convertToSnakeCase(input) {
+  if (typeof input === 'string') {
+    return input.replace(/([A-Z])/g, '_$1').toLowerCase();
+  } else if (Array.isArray(input)) {
+    return input.map(item => convertToSnakeCase(item));
+  } else if (typeof input === 'object' && input !== null) {
+    let newObj = {};
+    for (let key in input) {
+      let newKey = key.replace(/([A-Z])/g, '_$1').toLowerCase();
+      newObj[newKey] = convertToSnakeCase(input[key]);
+    }
+    return newObj;
   } else {
-    return convertToCamelCase(input);
+    return input;
   }
+}
+
+function convertToPascalCase(input) {
+  if (typeof input === 'string') {
+    return input.replace(/[-_ ](\w)/g, match => match.charAt(1).toUpperCase());
+  } else if (Array.isArray(input)) {
+    return input.map(item => convertToPascalCase(item));
+  } else if (typeof input === 'object' && input !== null) {
+    let newObj = {};
+    for (let key in input) {
+      let newKey = key.replace(/_([a-z])/g, (match, group1) => group1.toUpperCase());
+      newObj[newKey] = convertToPascalCase(input[key]);
+    }
+    return newObj;
+  } else {
+    return input;
+  }
+}
+
+function convertToKebabCase(input) {
+  if (typeof input === 'string') {
+    return input.replace(/_/g, '-').toLowerCase();
+  } else if (Array.isArray(input)) {
+    return input.map(item => convertToKebabCase(item));
+  } else if (typeof input === 'object' && input !== null) {
+    let newObj = {};
+    for (let key in input) {
+      let newKey = key.replace(/_([a-z])/g, (match, group1) => group1.toUpperCase());
+      newObj[newKey] = convertToKebabCase(input[key]);
+    }
+    return newObj;
+  } else {
+    return input;
+  }
+}
+
+module.exports = {
+  convertToCamelCase,
+  convertToSnakeCase,
+  convertToPascalCase,
+  convertToKebabCase
 };
